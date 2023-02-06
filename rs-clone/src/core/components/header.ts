034c0bd4f92216fcdf1b26040eb77154c64d021e
Kotkiller <kotkiller@inbox.ru>
2023-02-06 10:24:48 +0300

    feat:markup is almost ready

--- rs-clone/src/core/components/header.ts
+++ rs-clone/src/core/components/header.ts
@@ -8,7 +8,29 @@ class Header extends Component {
   constructor(className: string) {
     super(className);
     this.content = `
-      <h2>HEADER</h2>
+        <div class="header-content">
+      <div class="logo">
+        <h2><a href="#">Trello-clone</a></h2>
+      </div>
+      <nav class="nav" >
+        <button class="nav-button">
+          <span class="workspace">Рабочее пространство</span>
+          <span class="arrow-down"><img src="../../assets/down-arrow.svg" alt="arrow-down"></span>
+        </button>
+        <button class="nav-button">
+          <span class="recent">Недавние</span>
+          <span class="arrow-down"><img src="../../assets/down-arrow.svg" alt="arrow-down"></span>
+        </button>
+        <button class="nav-button">
+          <span class="favorite">В избранном</span>
+          <span class="arrow-down"><img src="../../assets/down-arrow.svg" alt="arrow-down"></span>
+        </button>      
+      </nav>
+      <button class="create-bord-button">Создать доску</button>
+      <div class="user">
+        <span>Y</span>
+       </div>
+    </div>
     `;
     this.position = 'afterbegin';
   }
