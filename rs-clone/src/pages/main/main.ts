41f561d2f9a91cda9adf827cd45c1d57850f8ca0
Veru44ia <vera3188@gmail.com>
2023-02-08 21:14:07 +0300

    feat: add aside

--- rs-clone/src/pages/main/main.ts
+++ rs-clone/src/pages/main/main.ts
@@ -1,25 +1,30 @@
 import Page from '../../core/templates/page';
 import Modal from '../../core/components/createBoardModal/createBoardModal';
+import Aside from '../../core/components/aside/aside';
 import './main.css';
 
 class MainPage extends Page {
   modal: Modal;
 
+  aside: Aside;
+
   constructor() {
     super();
     this.modal = new Modal('modal-container');
+    this.aside = new Aside();
   }
 
   renderBoardsList() {
-    this.container.innerHTML = '';
+    const mainWrapper = document.createElement('div');
+    mainWrapper.classList.add('main-container');
     const boardsBlock = document.createElement('div');
     boardsBlock.classList.add('boards-container');
     boardsBlock.innerHTML = `
       <button type="button" class="button button-create-board">Создать доску</button>    
     `;
-    (this.container as HTMLElement).append(boardsBlock);
+    mainWrapper.append(this.aside.render(), boardsBlock);
+    (this.container as HTMLElement).append(mainWrapper);
     return this.container;
-    
   }
 
   render() {
