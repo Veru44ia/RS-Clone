0edb08839923ab755e3bd63b91944366c41222fa
rannue <yavianbo@gmail.com>
2023-02-17 21:34:25 +0300

    feat: add mongoose connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("connected to db"));
 
 app.listen(3000, () => console.log("server started"));
